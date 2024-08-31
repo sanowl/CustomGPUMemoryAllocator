0f8005251b65e7966011152d7605b2d137d8a0f0
San <99511815+sanowl@users.noreply.github.com>
2024-08-31 14:26:06 +0300

    Update memory_manager.py

--- CustomGPUMemoryAllocator/src/memory_manager.py
+++ CustomGPUMemoryAllocator/src/memory_manager.py
@@ -1,12 +1,16 @@
 import numpy as np
 from collections import defaultdict
 from bisect import bisect_left
+import threading
+import math
 
 class MemoryBlock:
-    def __init__(self, start, size, is_free=True):
+    __slots__ = ['start', 'size', 'is_free', 'buddy']
+    def __init__(self, start, size, is_free=True, buddy=None):
         self.start = start
         self.size = size
         self.is_free = is_free
+        self.buddy = buddy
 
 class MemoryManager:
     def __init__(self, total_size, min_block_size, max_block_size):
@@ -14,77 +18,120 @@ class MemoryManager:
         self.min_block_size = min_block_size
         self.max_block_size = max_block_size
         self.blocks = [MemoryBlock(0, total_size)]
-        self.free_blocks = defaultdict(list)
-        self.free_blocks[total_size].append(0)
+        self.free_lists = [[] for _ in range(self._get_num_size_classes())]
+        self.allocated_blocks = {}
+        self.lock = threading.Lock()
+        self._initialize_buddy_system()
+
+    def _initialize_buddy_system(self):
+        size = self.total_size
+        while size >= self.min_block_size:
+            self._split_block(self.blocks[0])
+            size //= 2
+
+    def _get_num_size_classes(self):
+        return int(math.log2(self.max_block_size // self.min_block_size)) + 1
+
+    def _get_size_class(self, size):
+        return min(max(0, int(math.log2(size)) - int(math.log2(self.min_block_size))),
+                   self._get_num_size_classes() - 1)
+
+    def _split_block(self, block):
+        if block.size <= self.min_block_size:
+            return
+
+        size_class = self._get_size_class(block.size)
+        if size_class == 0:
+            return
+
+        new_size = block.size // 2
+        buddy = MemoryBlock(block.start + new_size, new_size, True, block)
+        block.size = new_size
+        block.buddy = buddy
+
+        self.blocks.insert(self.blocks.index(block) + 1, buddy)
+        self._add_to_free_list(block)
+        self._add_to_free_list(buddy)
+
+    def _merge_buddies(self, block):
+        if not block.buddy or not block.buddy.is_free:
+            return block
+
+        if block.start > block.buddy.start:
+            block, block.buddy = block.buddy, block
+
+        merged = MemoryBlock(block.start, block.size * 2, True)
+        self.blocks.remove(block.buddy)
+        self.blocks[self.blocks.index(block)] = merged
+        
+        self._remove_from_free_list(block)
+        self._remove_from_free_list(block.buddy)
+        self._add_to_free_list(merged)
+
+        return self._merge_buddies(merged)
+
+    def _add_to_free_list(self, block):
+        size_class = self._get_size_class(block.size)
+        self.free_lists[size_class].append(block)
+
+    def _remove_from_free_list(self, block):
+        size_class = self._get_size_class(block.size)
+        self.free_lists[size_class].remove(block)
 
     def allocate(self, size):
-        size_class = self._get_size_class(size)
-        for block_size in range(size_class, self.max_block_size + 1):
-            if self.free_blocks[block_size]:
-                block_start = self.free_blocks[block_size].pop()
-                block = next(b for b in self.blocks if b.start == block_start)
-                if block.size > size:
-                    remaining_size = block.size - size
-                    new_block = MemoryBlock(block.start + size, remaining_size)
-                    self.blocks.insert(self.blocks.index(block) + 1, new_block)
-                    self._add_to_free_blocks(new_block)
-                block.size = size
-                block.is_free = False
-                return block.start
-        return None
+        with self.lock:
+            size_class = self._get_size_class(size)
+            for i in range(size_class, self._get_num_size_classes()):
+                if self.free_lists[i]:
+                    block = self.free_lists[i].pop()
+                    while block.size > size and block.size > self.min_block_size:
+                        self._split_block(block)
+                    block.is_free = False
+                    self.allocated_blocks[block.start] = block
+                    return block.start
+            raise MemoryError("Not enough memory to allocate")
 
     def free(self, address):
-        block_index = self._find_block(address)
-        if block_index != -1:
-            block = self.blocks[block_index]
+        with self.lock:
+            if address not in self.allocated_blocks:
+                raise ValueError("Invalid address")
+            block = self.allocated_blocks.pop(address)
             block.is_free = True
-            self._add_to_free_blocks(block)
-            self._merge_adjacent_blocks(block_index)
-        else:
-            raise ValueError("Invalid address")
+            merged_block = self._merge_buddies(block)
+            self._add_to_free_list(merged_block)
 
     def get_free_blocks(self):
         return np.array([(block.start, block.size) for block in self.blocks if block.is_free])
 
-    def update_after_defragmentation(self, free_blocks):
-        self.blocks = [MemoryBlock(start, size, True) for start, size in free_blocks]
-        self.free_blocks.clear()
-        for block in self.blocks:
-            self._add_to_free_blocks(block)
+    def defragment(self):
+        with self.lock:
+            self.blocks.sort(key=lambda x: x.start)
+            i = 0
+            while i < len(self.blocks) - 1:
+                if self.blocks[i].is_free and self.blocks[i+1].is_free:
+                    self.blocks[i].size += self.blocks[i+1].size
+                    self._remove_from_free_list(self.blocks[i])
+                    self._remove_from_free_list(self.blocks[i+1])
+                    del self.blocks[i+1]
+                    self._add_to_free_list(self.blocks[i])
+                else:
+                    i += 1
 
-    def update_after_coalescing(self, free_blocks):
-        self.blocks = [b for b in self.blocks if not b.is_free]
-        self.blocks.extend([MemoryBlock(start, size, True) for start, size in free_blocks])
-        self.blocks.sort(key=lambda x: x.start)
-        self.free_blocks.clear()
-        for block in self.blocks:
-            if block.is_free:
-                self._add_to_free_blocks(block)
-
-    def _get_size_class(self, size):
-        return max(self.min_block_size, 1 << (size - 1).bit_length())
-
-    def _add_to_free_blocks(self, block):
-        size_class = self._get_size_class(block.size)
-        self.free_blocks[size_class].append(block.start)
+    def get_memory_usage(self):
+        total = self.total_size
+        used = sum(block.size for block in self.blocks if not block.is_free)
+        return {
+            "total": total,
+            "used": used,
+            "free": total - used,
+            "fragmentation": 1 - (max(block.size for block in self.blocks if block.is_free) / (total - used))
+        }
 
     def _find_block(self, address):
         return bisect_left(self.blocks, address, key=lambda b: b.start)
 
-    def _merge_adjacent_blocks(self, index):
-        while index > 0 and self.blocks[index - 1].is_free:
-            self.blocks[index - 1].size += self.blocks[index].size
-            self._remove_from_free_blocks(self.blocks[index])
-            del self.blocks[index]
-            index -= 1
-        while index < len(self.blocks) - 1 and self.blocks[index + 1].is_free:
-            self.blocks[index].size += self.blocks[index + 1].size
-            self._remove_from_free_blocks(self.blocks[index + 1])
-            del self.blocks[index + 1]
-        self._remove_from_free_blocks(self.blocks[index])
-        self._add_to_free_blocks(self.blocks[index])
+    def __str__(self):
+        return f"MemoryManager(total={self.total_size}, used={self.get_memory_usage()['used']}, free={self.get_memory_usage()['free']})"
 
-    def _remove_from_free_blocks(self, block):
-        size_class = self._get_size_class(block.size)
-        if block.start in self.free_blocks[size_class]:
-            self.free_blocks[size_class].remove(block.start)
+    def __repr__(self):
+        return self.__str__()
